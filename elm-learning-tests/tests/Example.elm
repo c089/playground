f7064d642cfb65332b43cb42b4821026b620fec8
Christoph Neuroth <christoph.neuroth@gmail.com>
2017-07-01 10:56:00 +0200

    Add learning tests

---
+++ elm-learning-tests/tests/Example.elm
@@ -0,0 +1,22 @@
+module Example exposing (..)
+
+import Expect exposing (Expectation)
+import Test exposing (..)
+import Tuple exposing (mapFirst, mapSecond)
+
+
+suite : Test
+suite =
+    let
+        addOne =
+            (\x -> x + 1)
+
+        inputTuple =
+            ( 0, 0 )
+    in
+        describe "Tuple"
+            [ test "can map first value" <|
+                \_ -> Expect.equal ( 1, 0 ) (mapFirst addOne inputTuple)
+            , test "can map second value" <|
+                \_ -> Expect.equal ( 0, 1 ) (mapSecond addOne inputTuple)
+            ]
