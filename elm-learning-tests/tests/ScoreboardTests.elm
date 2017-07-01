44e2235ed1c462ea560859d8b3fe38cdc9c3836c
Christoph Neuroth <christoph.neuroth@gmail.com>
2017-07-01 12:03:08 +0200

    Extract assertion

--- elm-learning-tests/tests/ScoreboardTests.elm
+++ elm-learning-tests/tests/ScoreboardTests.elm
@@ -1,10 +1,15 @@
 module ScoreboardTests exposing (suite)
 
-import Expect
 import Test exposing (Test, describe, test)
 import Test.Html.Query as Query
 import Test.Html.Selector exposing (text, tag, class)
 import Scoreboard exposing (..)
+
+
+expectScoreToBe expectedScore html =
+    Query.fromHtml html
+        |> Query.find [ class "score" ]
+        |> Query.has [ text expectedScore ]
 
 
 suite : Test
@@ -12,20 +17,13 @@ suite =
     describe "Scoreboard"
         [ test "initializes at 0:0" <|
             \_ ->
-                view initialModel
-                    |> Query.fromHtml
-                    |> Query.find [ class "score" ]
-                    |> Query.has [ text "0:0" ]
+                expectScoreToBe "0:0" (view initialModel)
         , test "allows increasing the score for team A" <|
             \_ ->
-                view (update (IncrementScore) (update (SelectTeam A) initialModel))
-                    |> Query.fromHtml
-                    |> Query.find [ class "score" ]
-                    |> Query.has [ text "1:0" ]
+                expectScoreToBe "1:0"
+                    (view (update (IncrementScore) (update (SelectTeam A) initialModel)))
         , test "allows increasing the score for team B" <|
             \_ ->
-                view (update (IncrementScore) (update (SelectTeam B) initialModel))
-                    |> Query.fromHtml
-                    |> Query.find [ class "score" ]
-                    |> Query.has [ text "0:1" ]
+                expectScoreToBe "0:1"
+                    (view (update (IncrementScore) (update (SelectTeam B) initialModel)))
         ]
