537497e839a10159b7026bebd8e46b4b7987c71a
Christoph Neuroth <christoph.neuroth@gmail.com>
2017-07-01 12:21:32 +0200

    Extract more duplication

--- elm-learning-tests/tests/ScoreboardTests.elm
+++ elm-learning-tests/tests/ScoreboardTests.elm
@@ -1,15 +1,12 @@
 module ScoreboardTests exposing (suite)
 
+import Expect exposing (Expectation)
+import Html exposing (Html)
 import Test exposing (Test, describe, test)
 import Test.Html.Query as Query
 import Test.Html.Selector exposing (text, tag, class)
+import List exposing (foldl)
 import Scoreboard exposing (..)
-
-
-expectScoreToBe expectedScore html =
-    Query.fromHtml html
-        |> Query.find [ class "score" ]
-        |> Query.has [ text expectedScore ]
 
 
 suite : Test
@@ -17,13 +14,26 @@ suite =
     describe "Scoreboard"
         [ test "initializes at 0:0" <|
             \_ ->
-                expectScoreToBe "0:0" (view initialModel)
+                expectScoreToBe "0:0"
+                    (afterReceiving [])
         , test "allows increasing the score for team A" <|
             \_ ->
                 expectScoreToBe "1:0"
-                    (view (update (IncrementScore) (update (SelectTeam A) initialModel)))
+                    (afterReceiving [ SelectTeam A, IncrementScore ])
         , test "allows increasing the score for team B" <|
             \_ ->
                 expectScoreToBe "0:1"
-                    (view (update (IncrementScore) (update (SelectTeam B) initialModel)))
+                    (afterReceiving [ SelectTeam B, IncrementScore ])
         ]
+
+
+expectScoreToBe : String -> Html Msg -> Expectation
+expectScoreToBe expectedScore html =
+    Query.fromHtml html
+        |> Query.find [ class "score" ]
+        |> Query.has [ text expectedScore ]
+
+
+afterReceiving : List Msg -> Html Msg
+afterReceiving messages =
+    view (foldl update initialModel messages)
