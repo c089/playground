d03b27f494d1fe8ce71da2c60406ddb78ff1c846
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-01-07 12:36:12 +0100

    Red: Test for calculate ("1")

--- ruby-learning-tests/ruby_spec.rb
+++ ruby-learning-tests/ruby_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe "StringCalculator" do
   it "returns 0 for empty strings" do
     expect(StringCalculator.new.calculate("")).to eq 0
   end
+
+  it "returns a single value converted to a number" do
+    expect(StringCalculator.new.calculate("1")).to eq 1
+  end
 end
