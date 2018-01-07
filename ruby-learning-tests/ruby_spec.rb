a2635bf54bc9daad62c737991787707294f8c888
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-01-07 13:25:01 +0100

    Refactor: Use delimitor to match kata description

--- ruby-learning-tests/ruby_spec.rb
+++ ruby-learning-tests/ruby_spec.rb
@@ -5,10 +5,10 @@ class StringCalculator
     else
       comma = input.index(',')
       newline = input.index("\n")
-      separator = comma || newline
-      if separator
-        first_number = input[0..separator-1]
-        remaining_input = input[separator+1..input.length]
+      delimitor = comma || newline
+      if delimitor
+        first_number = input[0..delimitor-1]
+        remaining_input = input[delimitor+1..input.length]
         calculate(first_number) + calculate(remaining_input)
       else
         input.to_i
