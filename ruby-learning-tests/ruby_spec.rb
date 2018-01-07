92487e26c9e1577c16df036d3ba2afccf7c054ef
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-01-07 12:39:09 +0100

    Red: Triangulate, high constant number

--- ruby-learning-tests/ruby_spec.rb
+++ ruby-learning-tests/ruby_spec.rb
@@ -16,4 +16,8 @@ RSpec.describe "StringCalculator" do
   it "returns a single value converted to a number" do
     expect(StringCalculator.new.calculate("1")).to eq 1
   end
+
+  it "returns a single big number" do
+    expect(StringCalculator.new.calculate("98765")).to eq 98765
+  end
 end
