6071ea2f57e5ae86ba34cf89603e081b3d4a712f
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-01-07 12:34:47 +0100

    Green: Fake It

--- ruby-learning-tests/ruby_spec.rb
+++ ruby-learning-tests/ruby_spec.rb
@@ -1,3 +1,9 @@
+class StringCalculator
+  def calculate(_)
+    0
+  end
+end
+
 RSpec.describe "StringCalculator" do
   it "returns 0 for empty strings" do
     expect(StringCalculator.new.calculate("")).to eq 0
