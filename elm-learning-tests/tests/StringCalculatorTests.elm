de662871aaa3544d29072c70661999e37ea5fb91
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-01-17 09:53:41 +0200

    Refactor to open for changes, change type to `Result`

--- elm-learning-tests/tests/StringCalculatorTests.elm
+++ elm-learning-tests/tests/StringCalculatorTests.elm
@@ -6,24 +6,24 @@ import Test exposing (Test, describe, test)
 
 add numbers =
     if numbers == "" then
-        0
+        Ok 0
     else if numbers == "1" then
-        1
+        Ok 1
     else if numbers == "2" then
-        2
+        Ok 2
     else
-        123456789
+        Ok 123456789
 
 
 suite : Test
 suite =
     describe "StringCalculator"
         [ test "returns 0 for empty string" <|
-            \_ -> Expect.equal 0 (add "")
+            \_ -> Expect.equal (Ok 0) (add "")
         , test "returns 1 for single number 1" <|
-            \_ -> Expect.equal 1 (add "1")
+            \_ -> Expect.equal (Ok 1) (add "1")
         , test "returns 2 for single number 2" <|
-            \_ -> Expect.equal 2 (add "2")
+            \_ -> Expect.equal (Ok 2) (add "2")
         , test "returns a single large number" <|
-            \_ -> Expect.equal 123456789 (add "123456789")
+            \_ -> Expect.equal (Ok 123456789) (add "123456789")
         ]
