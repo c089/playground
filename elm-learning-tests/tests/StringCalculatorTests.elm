42fe1f12a9f61b98ada0895d598f0818c920c511
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-01-17 09:36:35 +0200

    Red: add("1")

--- elm-learning-tests/tests/StringCalculatorTests.elm
+++ elm-learning-tests/tests/StringCalculatorTests.elm
@@ -13,4 +13,6 @@ suite =
     describe "StringCalculator"
         [ test "returns 0 for empty string" <|
             \_ -> Expect.equal 0 (add "")
+        , test "returns 1 for single number 1" <|
+            \_ -> Expect.equal 1 (add "1")
         ]
