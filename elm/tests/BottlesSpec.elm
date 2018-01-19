d4e3955032cf7cb65494c523e978c8d2661fd38d
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-01-19 14:11:56 +0200

    Green

--- elm/tests/BottlesSpec.elm
+++ elm/tests/BottlesSpec.elm
@@ -4,11 +4,22 @@ import Expect
 import Test exposing (Test, describe, test)
 
 
-verse _ =
-    "99 bottles of beer on the wall, "
-        ++ "99 bottles of beer.\n"
-        ++ "Take one down and pass it around, "
-        ++ "98 bottles of beer on the wall.\n"
+verse verse_number =
+    case verse_number of
+        99 ->
+            "99 bottles of beer on the wall, "
+                ++ "99 bottles of beer.\n"
+                ++ "Take one down and pass it around, "
+                ++ "98 bottles of beer on the wall.\n"
+
+        3 ->
+            "3 bottles of beer on the wall, "
+                ++ "3 bottles of beer.\n"
+                ++ "Take one down and pass it around, "
+                ++ "2 bottles of beer on the wall.\n"
+
+        _ ->
+            ""
 
 
 suite : Test
