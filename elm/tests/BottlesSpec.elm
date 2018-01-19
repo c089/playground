70f0c3775ef471b87347059314aa952afbd4f55f
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-01-19 14:18:43 +0200

    Green

--- elm/tests/BottlesSpec.elm
+++ elm/tests/BottlesSpec.elm
@@ -15,7 +15,8 @@ verse verse_number =
                 ++ " bottles of beer on the wall, "
                 ++ "99 bottles of beer.\n"
                 ++ "Take one down and pass it around, "
-                ++ "98 bottles of beer on the wall.\n"
+                ++ toString (verse_number - 1)
+                ++ " bottles of beer on the wall.\n"
 
         3 ->
             bottles
