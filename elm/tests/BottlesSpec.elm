aae94c934670d8555e82ec1324617537c042157b
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-01-19 14:09:25 +0200

    Red: Test another verse

--- elm/tests/BottlesSpec.elm
+++ elm/tests/BottlesSpec.elm
@@ -23,4 +23,13 @@ suite =
                         ++ "98 bottles of beer on the wall.\n"
                     )
                     (verse 99)
+        , test "another verse" <|
+            \_ ->
+                Expect.equal
+                    ("3 bottles of beer on the wall, "
+                        ++ "3 bottles of beer.\n"
+                        ++ "Take one down and pass it around, "
+                        ++ "2 bottles of beer on the wall.\n"
+                    )
+                    (verse 3)
         ]
