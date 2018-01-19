41deaf9ab97f3ca1da1954601015fe24b4f31416
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-01-19 14:06:44 +0200

    Green

--- elm/tests/BottlesSpec.elm
+++ elm/tests/BottlesSpec.elm
@@ -5,7 +5,10 @@ import Test exposing (Test, describe, test)
 
 
 verse _ =
-    ""
+    "99 bottles of beer on the wall, "
+        ++ "99 bottles of beer.\n"
+        ++ "Take one down and pass it around, "
+        ++ "98 bottles of beer on the wall.\n"
 
 
 suite : Test
