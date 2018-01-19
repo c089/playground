857f9f87df6ccdc1b9313483ade640e20ebf3712
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-01-19 14:19:43 +0200

    Green

--- elm/tests/BottlesSpec.elm
+++ elm/tests/BottlesSpec.elm
@@ -9,27 +9,13 @@ verse verse_number =
         bottles =
             toString verse_number
     in
-    case verse_number of
-        99 ->
-            bottles
-                ++ " bottles of beer on the wall, "
-                ++ bottles
-                ++ " bottles of beer.\n"
-                ++ "Take one down and pass it around, "
-                ++ toString (verse_number - 1)
-                ++ " bottles of beer on the wall.\n"
-
-        3 ->
-            bottles
-                ++ " bottles of beer on the wall, "
-                ++ bottles
-                ++ " bottles of beer.\n"
-                ++ "Take one down and pass it around, "
-                ++ toString (verse_number - 1)
-                ++ " bottles of beer on the wall.\n"
-
-        _ ->
-            ""
+    bottles
+        ++ " bottles of beer on the wall, "
+        ++ bottles
+        ++ " bottles of beer.\n"
+        ++ "Take one down and pass it around, "
+        ++ toString (verse_number - 1)
+        ++ " bottles of beer on the wall.\n"
 
 
 suite : Test
