11d751f6c49d1e0e581a6416c6150802952e2240
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-01-19 14:35:51 +0200

    Red: verses 2 to 0

--- elm/tests/BottlesSpec.elm
+++ elm/tests/BottlesSpec.elm
@@ -104,4 +104,23 @@ suite =
                         ++ "97 bottles of beer on the wall.\n"
                     )
                     (verses 99 98)
+        , test "the last verses" <|
+            \_ ->
+                Expect.equal
+                    ("2 bottles of beer on the wall, "
+                        ++ "2 bottles of beer.\n"
+                        ++ "Take one down and pass it around, "
+                        ++ "1 bottle of beer on the wall.\n"
+                        ++ "\n"
+                        ++ "1 bottle of beer on the wall, "
+                        ++ "1 bottle of beer.\n"
+                        ++ "Take it down and pass it around, "
+                        ++ "no more bottles of beer on the wall.\n"
+                        ++ "\n"
+                        ++ "No more bottles of beer on the wall, "
+                        ++ "no more bottles of beer.\n"
+                        ++ "Go to the store and buy some more, "
+                        ++ "99 bottles of beer on the wall.\n"
+                    )
+                    (verses 2 0)
         ]
