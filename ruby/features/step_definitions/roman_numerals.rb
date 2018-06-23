acec2b7f8718ec9bea03c8e2dd3e47a51dfdc708
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-06-23 11:50:28 +0200

    Extract method

--- ruby/features/step_definitions/roman_numerals.rb
+++ ruby/features/step_definitions/roman_numerals.rb
@@ -2,17 +2,17 @@ Given("the arabic number {int}") do |int|
   @number = int
 end
 
-symbols = {
-  1 => "I",
-  2 => "II",
-  3 => "III",
-  5 => "V",
-  6 => "VI",
-  7 => "VII",
-  10 => "X"
-}
+def to_roman
+  symbols = {
+    1 => "I",
+    2 => "II",
+    3 => "III",
+    5 => "V",
+    6 => "VI",
+    7 => "VII",
+    10 => "X"
+  }
 
-When("I convert it to roman") do
   if @number == 11
     @result = symbols[10] + symbols[1]
   elsif @number == 12
@@ -22,6 +22,10 @@ When("I convert it to roman") do
   else
     @result = symbols[@number]
   end
+end
+
+When("I convert it to roman") do
+  to_roman
 end
 
 Then("I should get {string}") do |expected_result|
