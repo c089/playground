e6af07ab4ba03f815c90b29c32498399574d76b6
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-10-18 09:49:58 +0200

    remove rdoc and ri from build
    rdoc not available in CI, also wasted time

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
 cd ruby
-gem install --user-install bundler
+gem install --no-rdoc --no-ri --user-install bundler
 bundle install
 bundle exec rspec *_spec.rb
 bundle exec cucumber --tags "not @ignore"
