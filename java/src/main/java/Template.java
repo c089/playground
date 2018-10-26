3e138ec0a195427b59febe0144f0e7ed95774bad
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-10-26 14:06:41 +0200

    Green

--- java/src/main/java/Template.java
+++ java/src/main/java/Template.java
@@ -1,12 +1,13 @@
 public class Template {
+    private final String template;
     private String value;
 
     public Template(String s) {
-        
+        template = s;
     }
 
     public String evaluate() {
-        return "Hello, " + this.value;
+        return this.template.replace("${name}", this.value);
     }
 
     public void set(String name, String value) {
