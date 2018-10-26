2629521b4df0d2fcdd060d9e5fc56bd8dd73444d
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-10-26 14:15:37 +0200

    Green: introduce map

--- java/src/main/java/Template.java
+++ java/src/main/java/Template.java
@@ -1,16 +1,29 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
 public class Template {
     private final String template;
     private String value;
+    private final Map<String, String> values = new HashMap<>();
 
     public Template(String s) {
         template = s;
     }
 
     public String evaluate() {
-        return this.template.replace("${name}", this.value);
+        String result = this.template;
+        for (Map.Entry<String, String> entry : this.values.entrySet()) {
+            String k = entry.getKey();
+            String v = entry.getValue();
+            result = result.replace("${"+k+"}", v);
+        }
+        return result;
     }
 
     public void set(String name, String value) {
         this.value = value;
+        this.values.put(name, value);
     }
 }
