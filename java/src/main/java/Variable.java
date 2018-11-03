6659ae5933ec4ed9af48fd9c4952c26b003eec65
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-11-03 14:18:30 +0100

    Green: parseSegments

--- java/src/main/java/Variable.java
+++ java/src/main/java/Variable.java
@@ -1,4 +1,11 @@
 public class Variable implements Segment {
+    private String name;
+
     public Variable(String name) {
+        this.name = name;
+    }
+
+    public boolean equals(Object other) {
+        return name.equals(((Variable) other).name);
     }
 }
