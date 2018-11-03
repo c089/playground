a9686cc08f5690008754b05da616d8015c1eb0f9
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-11-03 13:56:11 +0100

    Green: extract method

--- java/src/test/java/TemplateParseTest.java
+++ java/src/test/java/TemplateParseTest.java
@@ -8,16 +8,19 @@ public class TemplateParseTest {
     @Test
     public void emptyTemplateRendersAsEmptyString() throws Exception {
         List<String> segments = parse("");
-        assertEquals(1, segments.size(), "Number of segments");
-        assertEquals("", segments.get(0));
+        assertSegments(segments, "");
     }
 
 
     @Test
     public void templateWithOnlyPlainText() throws Exception {
         List<String> segments = parse("plain text only");
+        assertSegments(segments, "plain text only");
+    }
+
+    private void assertSegments(List<String> segments, String s) {
         assertEquals(1, segments.size(), "Number of segments");
-        assertEquals("plain text only", segments.get(0));
+        assertEquals(s, segments.get(0));
     }
 
     private List<String> parse(String s) {
