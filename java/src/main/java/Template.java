aa173fd9f6eebb025437c3f8ac33bcb0e64693c9
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-11-03 14:03:51 +0100

    Green: remove dead code

--- java/src/main/java/Template.java
+++ java/src/main/java/Template.java
@@ -1,14 +1,9 @@
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiConsumer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Template {
     private final String template;
-    private String value;
     private final Map<String, String> values = new HashMap<>();
 
     public Template(String s) {
@@ -37,23 +32,7 @@ public class Template {
         }
     }
 
-    private String replaceVariables() {
-        String result = this.template;
-        for (var entry : this.values.entrySet()) {
-            result = result.replace("${" + entry.getKey() + "}", entry.getValue());
-        }
-        return result;
-    }
-
-    private void checkForMissingValues(String result) throws MissingValueException {
-        Matcher m = Pattern.compile("\\$\\{.+\\}").matcher(result);
-        if (m.find()) {
-            throw new MissingValueException("No value for " + m.group());
-        }
-    }
-
     public void set(String name, String value) {
-        this.value = value;
         this.values.put(name, value);
     }
 }
